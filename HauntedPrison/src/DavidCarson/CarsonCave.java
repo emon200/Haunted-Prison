ec5b46be6c4b80a3ca60106d487d68be26555704
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-11-09 10:53:37 -0500

    a

--- HauntedPrison/src/DavidCarson/CarsonCave.java
+++ HauntedPrison/src/DavidCarson/CarsonCave.java
@@ -5,7 +5,8 @@ import caveExplorer.CaveRoom;
 
 public class CarsonCave extends CaveRoom{
 	
-	private boolean chatting;
+	private boolean visited = true;
+	private boolean chatting = true;
 	
 	public CarsonCave(String description) {
 		super(description);
@@ -21,7 +22,11 @@ public class CarsonCave extends CaveRoom{
 	
 	public void performAction(int direction) {
 		if(direction == 4) {
-			CaveExplorer.print("You have found a guard room.");
+			if(!visited) {
+				interact();
+			}else {
+				CaveExplorer.print("This is the room where you killed the zombie");
+			}
 		}else {
 			CaveExplorer.print("That key does nothing");
 		}
@@ -30,14 +35,15 @@ public class CarsonCave extends CaveRoom{
 	private void interact() {
 		CaveExplorer.print("In this room you find a handgun and a zombie slowly aproaching you." + "\n" + "Press e to pick up the gun");
 		String s = CaveExplorer.in.nextLine();
-		chatting = true;
 		while(chatting) {
 			if(!s.equals("e")) {
 				CaveExplorer.print("You picked up the gun and shot the zombie");
+				visited = true;
 			}else {
-				CaveExplorer.print("You fumbled the gun and the zombie killed you");
-				CaveExplorer.inventory.setHp(0);
+				CaveExplorer.print("You fumbled the gun and the zombie hurt you");
+				CaveExplorer.inventory.setHp(CaveExplorer.inventory.getHp() - 10);
 			}
+			chatting = false;
 		}		
 	}
 	
