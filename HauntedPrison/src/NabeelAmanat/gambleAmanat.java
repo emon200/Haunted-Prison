f719032981bc1d78de33e4fce369cc026c4ae9a6
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2017-11-09 11:42:32 -0500

    hi

--- HauntedPrison/src/NabeelAmanat/gambleAmanat.java
+++ HauntedPrison/src/NabeelAmanat/gambleAmanat.java
@@ -2,9 +2,6 @@ package NabeelAmanat;
 
 import caveExplorer.CaveExplorer;
 import caveExplorer.CaveRoom;
-import caveExplorer.Door;
-import caveExplorer.NPC;
-import caveExplorer.NPCRoom;
 
 public class gambleAmanat extends CaveRoom {
 
@@ -12,11 +9,10 @@ public class gambleAmanat extends CaveRoom {
 	boolean visited;
 	private String activeDescription;
 	private String inactiveDescription;
-	private boolean chatting;
 	
 	public gambleAmanat(String description) {
 		super(description);
-		this.activeDescription = "You feel a powerful aura, as if fate itself is calling you.";
+		this.activeDescription = "You remember the days you wold come here to gamble, how about one last shot?";
 		this.inactiveDescription = "This is where you took your chances.";
 		this.visited = false;
 	}
@@ -36,42 +32,43 @@ public class gambleAmanat extends CaveRoom {
 	}
 	
 	private void interact() {
-		int x = 0;
-		int hp = caveExplorer.Inventory.hp;
-			CaveExplorer.print("Tarot cards start to form around you, do you dare take one and read your fortune?");
+		double x = 0;
+		int hp = CaveExplorer.inventory.getHp();
+		if(visited == false) {
+			CaveExplorer.print("On a table you see poker cards forming, will you pick a card?");
 			String s = CaveExplorer.in.nextLine();
-			chatting = true;
-			while(chatting) {
 					if(s.equalsIgnoreCase("e")) {
+						x = (double)(Math.random()*5);
+						System.out.print(x);
 					CaveExplorer.print("As fate has it you...");
-					x = (int)((Math.random())*5);
-					if(x < 1) {
+					if(x <= 1) {
 						CaveExplorer.print("...have been gravely injured.");
 						CaveExplorer.inventory.setHp(hp-50);
 						visited = true;
-					}
-					else if(x > 1 && x < 2) {
+						}
+					else if(x > 1 && x <= 2) {
 						CaveExplorer.print("...are completely rejuvenated.");
 						CaveExplorer.inventory.setHp(100);
 						visited = true;
 					}
-					else if(x > 2 && x < 3) {
+					else if(x > 2 && x <= 3) {
 						CaveExplorer.print("...are not to be fought with.");
 						caveExplorer.Inventory.hasWeapon = true;
 						visited = true;
 					}
-					else if(x > 3 && x < 4) {
+					else if(x > 3 && x <= 4) {
 						CaveExplorer.print("...are dead.");
 						CaveExplorer.inventory.setHp(0);
 						visited = true;
 					}
-					else if(x > 4 && x < 5) {
-						CaveExplorer.print("...are unlucky.");
+					else if(x > 4 && x <= 5) {
+						CaveExplorer.print("...get nothing.");
 						visited = true;
 					}
 					}
-				}
 			}
+	}
+			
 
 		    
 	
@@ -101,41 +98,6 @@ public class gambleAmanat extends CaveRoom {
 		return activeDescription;
 	}
 	
-	public static void setUpCaves() {
-		//ALL OF THIS CODE CAN BE CHANGED
-		//1. Decide how big your caves should be
-		CaveExplorer.caves = new NPCRoom[5][5];
-		//2. Populate with caves and a default description: hint: when starting, use coordinates (helps debugging)
-		for(int row = 0; row < CaveExplorer.caves.length; row++) {
-			//PLEASE PAY ATTENTION TO THE DIFFERENCE:
-			for(int col = 0; col < CaveExplorer.caves[row].length; col++) {
-				//create a "default" cave
-				CaveExplorer.caves[row][col] = 
-						new NPCRoom("This cave has coords ("+row+","+col+")");
-			}
-		}
-		//3. Replace default rooms with custom rooms
-		//--- WE WILL DO LATER
-		CaveExplorer.npcs = new NPC[1];
-		CaveExplorer.npcs[0] = new NPC();
-		CaveExplorer.npcs[0].setposition(1, 1);
-		//ADD EACH PERSON's ROOM LIKE THIS:
-		CaveRoom customRoom = new NPCRoom("Room");
-		CaveExplorer.caves[4][3] = customRoom;
-		//4. Set your starting room:
-		CaveExplorer.currentRoom = CaveExplorer.caves[0][1];
-		CaveExplorer.currentRoom.enter();
-		//5. Set up doors
-		CaveRoom[][] c = CaveExplorer.caves;
-		c[0][1].setConnection(SOUTH, c[1][1], new Door());
-		/**
-		 * Special requests:
-		 * moving objects in caves
-		 * what happens when you lose?
-		 * can another object move toward you?
-		 */
-
-	}
 
 }
 
